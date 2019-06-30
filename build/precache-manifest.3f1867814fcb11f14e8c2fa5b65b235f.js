010edc50f9b4938b0f4f72533fdbf4d10bed4a1f
Ingmar Ross <dooker@dooker.eu>
2019-06-30 10:18:45 +0300

    Prod build

---
+++ build/precache-manifest.3f1867814fcb11f14e8c2fa5b65b235f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170ad158fbb3bebd88443322c10fc4de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3521f1cdb664ca13a582",
+    "url": "/static/css/main.c3530e2f.chunk.css"
+  },
+  {
+    "revision": "3bcc3477be09ec00534e",
+    "url": "/static/js/2.35ea8972.chunk.js"
+  },
+  {
+    "revision": "3521f1cdb664ca13a582",
+    "url": "/static/js/main.9b4a4070.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e7c9175248096738eb425aef55d28255",
+    "url": "/static/media/cloud.e7c91752.svg"
+  },
+  {
+    "revision": "d4e192b0cd71c732d72acaffe8b45f1c",
+    "url": "/static/media/solar.d4e192b0.svg"
+  }
+]);
